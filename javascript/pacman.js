9c4fc473137b4da62cde891905261fe6a91d3ddf
astaley333 <astaley@pdx.edu>
2021-05-01 13:55:22 -0700

    Refactor css styles and JS world selector. Clean up.

--- javascript/pacman.js
+++ javascript/pacman.js
@@ -1,26 +1,23 @@
 import { MAP_WIDTH, MAP_HEIGHT, map1 } from "./modules/maps.js";
 
+let world = document.getElementById("world");
+
 function drawWorld(initMap) {
   for (let r = 0; r < MAP_WIDTH; ++r) /*rows*/ {
     for (let c = 0; c < MAP_HEIGHT; ++c) /*cols*/ {
       if (initMap[r][c] === 1) {
-        document.getElementById("world").innerHTML +=
-          "<div class='wall'></div>";
+        world.innerHTML += "<div class='wall'></div>";
       } else if (initMap[r][c] === 2) {
-        document.getElementById("world").innerHTML +=
-          "<div class='rose-red'></div>";
+        world.innerHTML += "<div class='rose rose-red'></div>";
       } else if (initMap[r][c] === 3) {
-        document.getElementById("world").innerHTML +=
-          "<div class='ground'></div>";
+        world.innerHTML += "<div class='ground'></div>";
       } else if (initMap[r][c] === 4) {
-        document.getElementById("world").innerHTML +=
-          "<div class='pacman-right'></div>";
+        world.innerHTML += "<div class='pacman pacman-right'></div>";
       } else if (initMap[r][c] === 5) {
-        document.getElementById("world").innerHTML +=
-          "<div class='beer'></div>";
+        world.innerHTML += "<div class='beer'></div>";
       }
     }
-    document.getElementById("world").innerHTML += "<br>";
+    world.innerHTML += "<br>";
   }
 }
 
