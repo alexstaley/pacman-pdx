fd466e9801b1789dcc02a08bd2d0f62eee8a28d1
Brian <brianhlangford@gmail.com>
2021-05-22 22:23:02 -0700

    added logon.html, logon.js

--- javascript/logon.js
+++ javascript/logon.js
@@ -3,16 +3,19 @@
 //https://www.youtube.com/watch?v=iKlWaUszxB4
 
 function logon() {
+  window.alert("entered Logon");
   var userEmail = document.getElementById("emailName").value;
   var userPassword = document.getElementById("passwordName").value;
   firebase
     .auth()
     .signInWithEmailAndPassword(userEmail, userPassword)
-    // .then((userCredential) => {
-    // Signed in
-    //   var user = userCredential.user;
-    // ...
-    //  })
+
+    .then((userCredential) => {
+      // Signed in
+      var user = userCredential.user;
+      window.alert("inside of signInWithEmailAndPassword");
+      // ...
+    })
     .catch((error) => {
       var errorCode = error.code;
       var errorMessage = error.message;
@@ -21,6 +24,7 @@ function logon() {
 }
 
 function createAccount(userEmail, userPassword) {
+  window.alert("inside of CreateAccount");
   firebase
     .auth()
     .createUserWithEmailAndPassword(userEmail, userPassword)
@@ -44,5 +48,33 @@ function logout() {
     })
     .catch((error) => {
       // An error happened.
+    });
+}
+
+function googleLog() {
+  window.alert("inside of googleLog");
+  var provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
     });
 }
