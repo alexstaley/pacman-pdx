f37332893ff66cfb99e076103d276fd524d82789
Cristina <cristinastone12@gmail.com>
2021-05-27 11:21:21 -0700

    Ordering high scores (need to get to descending)

--- javascript/leaderboard.js
+++ javascript/leaderboard.js
@@ -2,34 +2,47 @@
 // "9.3: Firebase: Retrieving Data - Programming with Text"
 // https://www.youtube.com/watch?v=NcewaPfFR6Y
 
+// Also referred to exercise 06-api.js from HW3 for appending to list
+
 let database = firebase.database();
 
 let ref = database.ref('leaderboard');
-ref.on('value', getData, errData);
+ref.on('value', getData, errData); 
 
 function getData(data){
     let scores = data.val();
     let keys = Object.keys(scores);
     console.log(keys);
 
-    let list = document.getElementById('leaderboard');
+    // TODO: sort in descending order (highest score to lowest)
 
+    let query = ref.orderByChild('highscore').limitToLast(3);
+    query.on('value', function(snapshot){
+        
+    })
+
+    let playerList = document.getElementById('leaderboard-player');
+    let scoreList = document.getElementById('leaderboard-score');
+
+    // TODO: change keys.length to 5 
     for (let i = 0; i < keys.length; i++){
         let k = keys[i];
         let name = scores[k].name;
         let score = scores[k].score;
-        let highscore = scores[k].highscore;
+        let highscore = scores[k].highscore
+            .toLocaleString()
 
-        // let li = document.createElement('li', name + " ------- " + highscore);
-        // li.parent('leaderboard');
-        let listitem = document.createElement('li');
-        listitem.textContent = `${name} - ${highscore}`;
-        list.appendChild(listitem);
-        console.log(name, highscore);
+        let playerListItem = document.createElement('li');
+        playerListItem.textContent = `${name}`;
+        playerList.appendChild(playerListItem);
+
+        let scoreListItem = document.createElement('dt');
+        scoreListItem.textContent = `${highscore}`;
+        scoreList.appendChild(scoreListItem);
     }
 }
 
-// If error retrieving data
+// If error in retrieving data
 function errData(err){
     console.log('Error!');
     console.log(err);
