d392efa7cf3e27d307c1a0c3c82fdacfdb5b0462
Cristina <cristinastone12@gmail.com>
2021-05-27 12:49:54 -0700

    Got ordering to work for leaderboard

--- javascript/leaderboard.js
+++ javascript/leaderboard.js
@@ -11,25 +11,25 @@ ref.on('value', getData, errData);
 
 function getData(data){
     let scores = data.val();
-    let keys = Object.keys(scores);
-    console.log(keys);
 
-    // TODO: sort in descending order (highest score to lowest)
-
-    let query = ref.orderByChild('highscore').limitToLast(3);
-    query.on('value', function(snapshot){
-        
-    })
+    let scoresArray = [];
+    for (let player in scores) {
+      scoresArray.push(scores[player]);
+    }
+    console.log(scoresArray);
+    let hiScores = scoresArray.sort((player1, player2) => {
+        return player2.highscore - player1.highscore;
+    });
+    console.log(hiScores);
 
     let playerList = document.getElementById('leaderboard-player');
     let scoreList = document.getElementById('leaderboard-score');
 
-    // TODO: change keys.length to 5 
-    for (let i = 0; i < keys.length; i++){
-        let k = keys[i];
-        let name = scores[k].name;
-        let score = scores[k].score;
-        let highscore = scores[k].highscore
+    for (let i = 0; i < 5; i++){
+        // let k = keys[i];
+        let name = hiScores[i].name;
+        console.log(name);
+        let highscore = hiScores[i].highscore
             .toLocaleString()
 
         let playerListItem = document.createElement('li');
