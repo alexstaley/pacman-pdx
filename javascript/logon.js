135525b247288580853ba7620640ffa8585328a7
Alex Staley <70488373+alexstaley@users.noreply.github.com>
2021-05-31 16:37:42 -0700

    Revert "Second login page edit"

--- javascript/logon.js
+++ javascript/logon.js
@@ -2,15 +2,22 @@
 //Firebase Web Login - Firebase Web App Tutorial
 //https://www.youtube.com/watch?v=iKlWaUszxB4
 
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  let currentPlayer = document.getElementById('playerFrame');
+  if(currentPlayer!=null)
+  {
+    currrentPlayer.innerHTML = profile.getName();
+  }
+}
+
 function signOut() {
   var auth2 = gapi.auth2.getAuthInstance();
   auth2.signOut().then(function () {
-    console.log("User signed out.");
-    location.assign("logon.html");
+    console.log('User signed out.');
   });
-}
-function onSuccess(googleUser) {
-  console.log("Logged in as: " + googleUser.getBasicProfile().getName());
-  let userName = googleUser.getBasicProfile().getGivenName();
-  location.assign("pacman.html");
 }
